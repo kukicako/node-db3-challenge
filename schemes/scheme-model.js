6c4ccd2d6cd1478c364fd0998ca3efdd94133bcf
Muamer Kukic <kukicako@gmail.com>
2019-11-06 16:47:33 -0800

    mvp i believe

---
+++ schemes/scheme-model.js
@@ -0,0 +1,45 @@
+const db = require('../data/dbconfig.js');
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+};
+
+function find() {
+  return db('schemes');
+}
+
+function findById(id) {
+  return db('schemes')
+    .where({ id })
+    .first();
+}
+
+function findSteps(schemeId) {
+  return db('steps as st')
+    .join('schemes as s', 's.id', 'st.scheme_id')
+    .where({ scheme_id: schemeId });
+}
+
+function add(scheme) {
+  return db('schemes')
+    .insert(scheme, 'id')
+    .then(([id]) => {
+      return findById(id);
+    });
+}
+function update(changes, id) {
+    return db('schemes')
+      .where({ id })
+      .update(changes);
+  }
+
+  function remove(id) {
+    return db('schemes')
+      .where('id', id)
+      .del();
+  }
